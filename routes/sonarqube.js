91f704e1b2af84b14494a4d81c7e9a38768efbe7
akintola <lonlondevelop@gmail.com>
2024-07-15 23:22:26 +0100

    fix: update sonarqube.js

--- routes/sonarqube.js
+++ routes/sonarqube.js
@@ -12,85 +12,83 @@ router.post('/', (req, res) => {
   const { serverUrl, project, qualityGate, analysedAt, status } = sonarqubePayload;
   const { conditions } = qualityGate;
   const color = qualityGate.status === 'OK' ? '#ff7b7b' : '#008080';
-  const statusIcon = qualityGate.status === 'OK' ? '✅' : '🚨';
   
 
   // Format the payload for Slack
   const slackPayload = {
     attachments: [
-        {
-            color,
-            blocks: [
-                {
-                  type: "header",
-                  text: {
-                    type: "plain_text",
-                    text: `${statusIcon} SonarQube Analysis for ${project.name} - ${qualityGate.status}`,
-                    emoji: true
-                  }
-                },
-                {
-                  type: "section",
-                  fields: [
-                    {
-                      type: "mrkdwn",
-                      text: `*Project:* ${project.name}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Status:* ${qualityGate.status}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Analyzed At:* ${new Date(analysedAt).toLocaleString()}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Project URL:* <${project.url}|${project.name}>`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Server URL:* <${serverUrl}|SonarQube>`
-                    }
-                  ]
-                },
-                {
-                  type: "section",
-                  text: {
-                    type: "mrkdwn",
-                    text: "*Quality Gate Conditions:*"
-                  }
-                },
-                ...conditions.map(condition => ({
-                  type: "section",
-                  fields: [
-                    {
-                      type: "mrkdwn",
-                      text: `*Metric:* ${condition.metric}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Status:* ${condition.status}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Operator:* ${condition.operator}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Value:* ${condition.value || 'N/A'}`
-                    },
-                    {
-                      type: "mrkdwn",
-                      text: `*Error Threshold:* ${condition.errorThreshold}`
-                    }
-                  ]
-                }))
-            ],
-            footer: "SonarQube",
-            // footer_icon: iconUrl,
-            ts: new Date(analysedAt).getTime() / 1000
-        }
+      {
+        color: color,
+        blocks: [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: `${qualityGate.status === 'OK' ? '✅' : '❌'} SonarQube Analysis for ${project.name} - ${qualityGate.status}`,
+              emoji: true
+            }
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Project:* ${project.name}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Status:* ${qualityGate.status}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Analyzed At:* ${new Date(analysedAt).toLocaleString()}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Project URL:* <${project.url}|${project.name}>`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Server URL:* <${serverUrl}|SonarQube>`
+              }
+            ]
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Quality Gate Conditions:*"
+            }
+          },
+          ...conditions.map(condition => ({
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Metric:* ${condition.metric}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Status:* ${condition.status}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Operator:* ${condition.operator}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Value:* ${condition.value || 'N/A'}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Error Threshold:* ${condition.errorThreshold}`
+              }
+            ]
+          }))
+        ],
+        footer: "SonarQube",
+        ts: new Date(analysedAt).getTime() / 1000
+      }
     ]
   };
 
